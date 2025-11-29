e683debb2d8dac7bd09b86771b6e3416ec38afbf
Mehmet Ali Sahin <1127177+mehmetali@users.noreply.github.com>
2025-11-29 20:21:35 +0000

    dfdf

--- mssql_backup_rust_service/src/app.rs
+++ mssql_backup_rust_service/src/app.rs
@@ -6,6 +6,28 @@ live_design! {
     use link::theme::*;
     use link::shaders::*;
     use link::widgets::*;
+
+    THEME_FONT_REGULAR = {
+        font_family:{
+            latin = {path: dep("IBMPlexSans-Text.ttf")},
+            chinese = {path: dep("LXGWWenKaiRegular.ttf")},
+            emoji = {path: dep("NotoColorEmoji.ttf")},
+        }
+    }
+    THEME_FONT_BOLD = {
+        font_family:{
+            latin = {path: dep("IBMPlexSans-SemiBold.ttf")},
+            chinese = {path: dep("LXGWWenKaiRegular.ttf")},
+            emoji = {path: dep("NotoColorEmoji.ttf")},
+        }
+    }
+    THEME_FONT_ITALIC = {
+        font_family:{
+            latin = {path: dep("IBMPlexSans-Italic.ttf")},
+            chinese = {path: dep("LXGWWenKaiRegular.ttf")},
+            emoji = {path: dep("NotoColorEmoji.ttf")},
+        }
+    }
 /*
     App = {{App}} {
         ui: <Window> {
@@ -88,27 +110,7 @@ impl LiveRegister for App {
             makepad_widgets::makepad_draw::shader::std::font_atlas::font_sdf;
             makepad_widgets::makepad_draw::font_loader::Font;
 
-            THEME_FONT_REGULAR = {
-                font_family:{
-                    latin = {path: dep("IBMPlexSans-Text.ttf")},
-                    chinese = {path: dep("LXGWWenKaiRegular.ttf")},
-                    emoji = {path: dep("NotoColorEmoji.ttf")},
-                }
-            }
-            THEME_FONT_BOLD = {
-                font_family:{
-                    latin = {path: dep("IBMPlexSans-SemiBold.ttf")},
-                    chinese = {path: dep("LXGWWenKaiRegular.ttf")},
-                    emoji = {path: dep("NotoColorEmoji.ttf")},
-                }
-            }
-            THEME_FONT_ITALIC = {
-                font_family:{
-                    latin = {path: dep("IBMPlexSans-Italic.ttf")},
-                    chinese = {path: dep("LXGWWenKaiRegular.ttf")},
-                    emoji = {path: dep("NotoColorEmoji.ttf")},
-                }
-            }
+
         }
     }
 }

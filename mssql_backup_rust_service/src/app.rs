1d2d283857a903c8da84fab1ab27dd8bcb13cdd7
Mehmet Ali Sahin <1127177+mehmetali@users.noreply.github.com>
2025-11-30 08:27:18 +0000

    gdfgdfg

--- mssql_backup_rust_service/src/app.rs
+++ mssql_backup_rust_service/src/app.rs
@@ -39,7 +39,7 @@ impl MatchEvent for App {
        //tracing::info!("AppMain::handle_event received event: {:?}", actions);
        let window = self.ui.window(&[id!(main_window)]);
         if self.ui.button(&[id!(quit_button)]).clicked(actions) {
-            ::info!("AppMain::handle_event received event: {:?}", self.ui.button(&[id!(quit_button)]));
+            tracing::info!("AppMain::handle_event received event: {:?}", self.ui.button(&[id!(quit_button)]));
         }
         if self.ui.button(&[id!(setup_button)]).clicked(actions) {
             log!("Setup button clicked!");
